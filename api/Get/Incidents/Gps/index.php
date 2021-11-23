c5f192e438ae2ea88a7261b0592eb8487206c130
Tyler Fox <tyler@tylerthefox.com>
2021-11-23 05:24:36 +0000

    Added heatmap functionality

--- api/Get/Incidents/Gps/index.php
+++ api/Get/Incidents/Gps/index.php
@@ -6,11 +6,11 @@ require_once("../../../../config.php");
 
 $con = pg_connect("host=$hostname dbname=$database user=$username password=$password") or die ("Could not connect to server\n");
 
-$query = "SELECT latitude, longitude FROM public.incident";
+$query = 'SELECT latitude AS "lat", longitude as "lng", count(*) AS "count" FROM public.incident group by latitude, longitude';
 
 $ret = pg_query($con, $query) or die("Cannot execute query: $query\n");
 $myarray = pg_fetch_all($ret);
 
-print(json_encode($myarray));
+print(json_encode($myarray, JSON_NUMERIC_CHECK));
 exit();
 ?>
--- js/AGENCY/map.js
+++ js/AGENCY/map.js
@@ -11,12 +11,55 @@ app.controller('mainCtrl', function ($scope, $http)
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         });
-
 	$scope.OR911_MAP.addTo($scope.map);
+
+	$scope.cfg = {
+	  // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+	  // if scaleRadius is false it will be the constant radius used in pixels
+	  "radius": 0.008,
+	  "maxOpacity": .8,
+	  // scales the radius based on map zoom
+	  "scaleRadius": true,
+	  // if set to false the heatmap uses the global maximum for colorization
+	  // if activated: uses the data maximum within the current map boundaries
+	  //   (there will always be a red spot with useLocalExtremas true)
+	  "useLocalExtrema": true,
+	  // which field name in your data represents the latitude - default "lat"
+	  latField: 'lat',
+	  // which field name in your data represents the longitude - default "lng"
+	  lngField: 'lng',
+	  // which field name in your data represents the data value - default "value"
+	  valueField: 'count'
+	};
+
+	$scope.heatmapLayer = new HeatmapOverlay($scope.cfg);
+	$scope.map.addLayer($scope.heatmapLayer);
     };
+
+    $scope.HeatUpTheMap = function()
+    {
+	var heatMapData = {
+  		data: $scope.GPSData
+	};
+
+	//var heatMapData = {
+  	//	data: [{lat: 24.6408, lng:46.7728, count: 3},{lat: 50.75, lng:-1.55, count: 1}]
+	//};
+
+	console.log(heatMapData);
+
+	$scope.heatmapLayer.setData(heatMapData);
+    }
 
     $scope.init = function (standalone)
     {
 	$scope.initMap();
+
+	// Let's download the GPS data from our api.
+        $http.get("./api/Get/Incidents/Gps/", {timeout: 5000}).then(function (response)
+        {
+		$scope.GPSData = response.data;
+		$scope.HeatUpTheMap();
+	});
     };
 });
